1da77c970c057e6d3e3ab50822708bb63e5745f0
enigma <auxiliary42@gmail.com>
2021-09-23 13:53:43 -0400

    Add files via upload

---
+++ Lab4/age.cpp
@@ -0,0 +1,46 @@
+// Guess the Age of the Troll
+// Stephen M. Struble
+// September 17, 2021
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using std::cout; using std::cin; using std::endl;
+
+int main() {
+	cout << "A fair young maiden is locked up in an enchanted castle guarded by an ugly old troll." << endl;
+	cout << "Once a year, a handsome prince is allowed to come to the castle to try to free the maiden." << endl;
+	cout << "To free her, the prince has to guess the troll's age." << endl;
+	cout << "If the prince guesses correctly, the maiden is freed, they get married and live happily ever after." << endl;
+	cout << "If the prince guesses incorrectly, the troll says if he is younger or older and the prince has to come next year." << endl;
+
+	srand(time(nullptr));
+
+	int lowestAge;
+	lowestAge = 100;
+	int highestAge;
+	highestAge = 200;
+	int trollAge;
+	trollAge = (highestAge - lowestAge + 1) + (rand() % (lowestAge + 1));
+	int guessAge;
+
+	do {
+		cout << "Hello, handsome prince. I am an ugly old troll. How old am I?" << endl;
+		cin >> guessAge;
+		if (guessAge == trollAge) {
+			cout << "You got it! Here is the fair maiden. Live happily ever after!" << endl;
+		}
+		else if (guessAge < trollAge) {
+			cout << "Wrong, I am older. No fair maiden for you. Better luck next year." << endl;
+			trollAge++;
+			guessAge = 0;
+		}
+		else {
+			cout << "Wrong, I am younger. No fair maiden for you. Better luck next year." << endl;
+			trollAge++;
+			guessAge = 0;
+		}
+	} while (guessAge != trollAge);
+
+}
---
+++ Lab4/calculator.cpp
@@ -0,0 +1,43 @@
+// Calculator
+// Stephen M. Struble
+// September 17, 2021
+
+
+#include <iostream>
+#include <cmath>
+
+using std::cout; using std::cin; using std::endl;
+
+int main() {
+	cout << "1. absolute value" << endl << "2. square root" << endl << "3. floor" << endl << "4. power" << endl;
+	int operation;
+	do {
+		cout << "Select an operation : ";
+		cin >> operation;
+		double Input1, Input2, Input3, Input4, InputExp;
+		switch (operation) {
+		case 1:
+			cout << "Enter a number : ";
+			cin >> Input1;
+			cout << "The result is " << abs(Input1) << endl;
+			break;
+		case 2:
+			cout << "Enter a number: ";
+			cin >> Input2;
+			cout << "The result is " << sqrt(Input2) << endl;
+			break;
+		case 3:
+			cout << "Enter a number: ";
+			cin >> Input3;
+			cout << "The result is " << floor(Input3) << endl;
+			break;
+		case 4:
+			cout << "Enter a number: ";
+			cin >> Input4;
+			cout << "Enter a power: ";
+			cin >> InputExp;
+			cout << "The result is " << pow(Input4, InputExp) << endl;
+			break;
+		}
+	} while (operation > 0 && operation < 5);
+}
