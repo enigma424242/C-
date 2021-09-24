1c70d38f9ff3a6043f126b1d71e33311dba50764
enigma <auxiliary42@gmail.com>
2021-09-24 10:26:34 -0400

    Delete Lab5_Figures directory

--- Lab5_Figures/figures.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef figures_hpp
-#define figures_hpp
-#endif figures_hpp
-
-void filledSquare(int size, char paint);
-void hollowSquare(int size, char paint);
-void slash(int size, char paint);
-void backSlash(int size, char paint);
-void diamond(int size, char paint);
