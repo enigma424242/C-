50bab3706944aac687b83845dbe52a28aaef5b11
enigma <auxiliary42@gmail.com>
2021-09-24 10:26:59 -0400

    Add files via upload

---
+++ Lab5/Lab5_Figures/figures.hpp
@@ -0,0 +1,9 @@
+#ifndef figures_hpp
+#define figures_hpp
+#endif figures_hpp
+
+void filledSquare(int size, char paint);
+void hollowSquare(int size, char paint);
+void slash(int size, char paint);
+void backSlash(int size, char paint);
+void diamond(int size, char paint);
